f9a6ecb199d9815daccadba887d9bd60eca341a6
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-10-24 11:22:00 +0200

    Add files via upload
    Vježbe petlje

---
+++ galavićMarkoPetlje/galavićMarkoPetlje/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //komentar u jednom retku
+
+            /*ovaj
+             * komentar
+             mogu
+             pisati*/
+
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst");
+
+            unosTeksta = Console.ReadLine();
+
+            Console.WriteLine(unosTeksta);
+
+            for (int i= 0; i< unosTeksta.Length; i++)
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i+1));
+            }
+
+
+            Console.ReadKey();
+
+            //Ispis:
+             /*V
+              * Vo
+              * Vod
+              * Voda*/
+
+
+        }
+    }
+}
