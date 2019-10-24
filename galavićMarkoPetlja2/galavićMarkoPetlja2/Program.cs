af4c4c19e0a1c7cf7405d88e6d4197927f05cf77
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-10-24 11:36:36 +0200

    Add files via upload

---
+++ galavićMarkoPetlja2/galavićMarkoPetlja2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetlja2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst");
+
+            unosTeksta = Console.ReadLine();
+
+            //Console.WriteLine(unosTeksta);
+
+            for (int i = unosTeksta.Length; i>0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0,i));
+            }
+
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
